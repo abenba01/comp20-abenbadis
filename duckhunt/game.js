cd364840ae6fd1626da40253fb7da04c26ffa7b2
Alexandra Benbadis <benbadass95@Alexandras-MacBook-Pro-3.local>
2015-10-07 16:47:12 -0400

    I finished Lab 5

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,19 @@
-// Replace this with your code...
+background = new Image();
+background.src = "duckhunt-background.gif";
+
+duck1 = new Image();
+duck1.src = "duckhunt_various_sheet.png";
+
+duck2 = new Image();
+duck2.src = "duckhunt_various_sheet.png";
+
+
+background.onload = function init () {
+	canvas = document.getElementById('game_canvas');
+	ctx = canvas.getContext('2d');	
+	ctx.drawImage(background,0,0, 800, 600);
+	ctx.drawImage(duck1, 250, 107, 48, 49, 200, 10, 200, 200);
+	ctx.drawImage(duck2, 75, 107, 40, 48, 500, 100, 200, 200);
+}
+
+
