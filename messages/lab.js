cf995a0d627240f6cf7e3dd87acb157a98c55834
Alexandra Benbadis <benbadass95@Alexandras-MacBook-Pro-3.local>
2015-10-18 12:59:28 -0400

    trying to show messages

--- messages/lab.js
+++ messages/lab.js
@@ -6,7 +6,7 @@ function parse() {
 	data = "data.json";	
 	elem = document.getElementById("messages");
 	for (count = 0; count < data.length; count++) {
-
+		console.log(Object.keys(data[count]));
 		elem.innerHTML += data[count]["content"] + data[count]["username"];
 
 	}
