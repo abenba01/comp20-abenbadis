aca0d7be9ff3e0adf6e0d79a513471659a3fe4bb
Alexandra Benbadis <benbadass95@Alexandras-MacBook-Pro-3.local>
2015-10-18 13:35:26 -0400

    trying to show messages

--- messages/lab.js
+++ messages/lab.js
@@ -1,13 +1,13 @@
 xhr = new XMLHttpRequest();
 xhr.open("get", "data.json", true);
 
-function parse() {
-	
-	elem = document.getElementById("messages");
-	for (count = 0; count < xhr.length; count++) {
-		console.log(Object.keys(xhr)[count]);
-		elem.innerHTML += xhr[count]["content"] + xhr[count]["username"];
 
+function parse() {
+	parsedObjects = JSON.parse(xhr);
+	elem = document.getElementById("messages");
+	for (count = 0; count < parsedObjects.length; count++) {
+		console.log(Object.keys(parsedObjects[count]));
+		elem.innerHTML += parsedObjects[count]["content"] + parsedObjects[count]["username"];
 	}
 
 }
