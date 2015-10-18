0fb5c2efd37043040e37f4ce2719ce2574d97d94
Alexandra Benbadis <benbadass95@Alexandras-MacBook-Pro-3.local>
2015-10-18 13:37:37 -0400

    trying to show messages

--- messages/lab.js
+++ messages/lab.js
@@ -1,9 +1,9 @@
 xhr = new XMLHttpRequest();
-xhr.open("get", "data.json", true);
+parsedObjects = xhr.open("get", "data.json", true);
 
 
 function parse() {
-	parsedObjects = JSON.parse(xhr);
+	
 	elem = document.getElementById("messages");
 	for (count = 0; count < parsedObjects.length; count++) {
 		console.log(Object.keys(parsedObjects[count]));
