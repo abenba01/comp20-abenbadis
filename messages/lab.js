57428b86581c92798db2c10f6ff5be0c1e861102
Alexandra Benbadis <benbadass95@Alexandras-MacBook-Pro-3.local>
2015-10-19 13:01:21 -0400

    part 1 works

--- messages/lab.js
+++ messages/lab.js
@@ -1,13 +1,23 @@
-xhr = new XMLHttpRequest();
-xhr.open("get", "data.json", true);
-
 function parse() {
-	
-	elem = document.getElementById("messages");
-	for (count = 0; count < xhr.length; count++) {
-		console.log(Object.keys(xhr[count]));
-		elem.innerHTML += xhr[count]["content"] + xhr[count]["username"];
+
+	xhr = new XMLHttpRequest();
+	xhr.open("get", "data.json", true);
+	xhr.onreadystatechange = function () {
+		if (( xhr.readyState == 4 ) && (xhr.status == 200)) {
+
+			parsedObjects = JSON.parse(xhr.responseText);
+			elem = document.getElementById("messages");
+
+			for ( count = 0; count < parsedObjects.length; count++){
+				console.log(Object.keys(parsedObjects[count]));
+				elem.innerHTML += "<h3>" + parsedObjects[count]["content"] + "</h3>" + "<p>" + parsedObjects[count]["username"] + "</p>";
+
+			}
+		}
+			
 	}
+	xhr.send();
+	
 }
 
 	
